f9ef3e5569a66c90eb0bdfba95f7cc8a3a412ab1
oliviaAB <olivia.angelinbonnet@gmail.com>
2024-01-16 14:55:17 +1300

    Copied data import chapter

--- _targets.R
+++ _targets.R
@@ -26,6 +26,73 @@ list(
     col_ids = c("marker", "gene_id", "sample_id"),
     features_as_rowss = c(TRUE, TRUE, FALSE),
     target_name_suffixes = c("geno", "transcripto", "metabo")
+  ),
+
+  ## Features metadata import
+  tar_target(
+    fmetadata_file_geno,
+    system.file("extdata/genomics_features_info.csv", package = "moiraine"),
+    format = "file"
+  ),
+
+  tar_target(
+    fmetadata_geno,
+    import_fmetadata_csv(
+      fmetadata_file_geno,
+      col_id = "marker",
+      col_types = c("chromosome" = "c")
+    )
+  ),
+
+  import_fmetadata_csv_factory(
+    files = c(
+      system.file("extdata/metabolomics_features_info.csv", package = "moiraine")
+    ),
+    col_ids = c("feature_id"),
+    target_name_suffixes = c("metabo")
+  ),
+
+  import_fmetadata_gff_factory(
+    files = system.file("extdata/bos_taurus_gene_model.gff3", package = "moiraine"),
+    feature_types = "genes",
+    add_fieldss = c("Name", "description"),
+    target_name_suffixes = "transcripto"
+  ),
+
+  ## Samples metadata import
+  import_smetadata_csv_factory(
+    files = system.file("extdata/samples_info.csv", package = "moiraine"),
+    col_ids = "animal_id",
+    target_name_suffixes = "all"
+  ),
+
+  ## Creating omics sets for each dataset
+  create_omics_set_factory(
+    datasets = c(data_geno, data_transcripto, data_metabo),
+    omics_types = c("genomics", "transcriptomics", "metabolomics"),
+    features_metadatas = c(fmetadata_geno, fmetadata_transcripto, fmetadata_metabo),
+    samples_metadatas = c(smetadata_all, smetadata_all, smetadata_all)
+  ),
+
+  ## Creating the MultiDataSet object
+  tar_target(
+    mo_set,
+    create_multiomics_set(
+      list(set_geno,
+           set_transcripto,
+           set_metabo)
+    )
+  ),
+
+  ## Example with names
+  tar_target(
+    mo_set_with_names,
+    create_multiomics_set(
+      list(set_geno,
+           set_transcripto,
+           set_metabo),
+      datasets_names = c("CaptureSeq", "RNAseq", "LCMS")
+    )
   )
 
 )
